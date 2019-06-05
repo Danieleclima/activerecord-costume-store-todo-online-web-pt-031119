fb6b59a9e0381245a78320de1abcd664ce491e07
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-05 17:28:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -19,16 +29,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "costumesstores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "number_of_employees"
-    t.string "in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
