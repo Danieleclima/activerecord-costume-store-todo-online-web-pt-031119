5a37dba90e6a121745eaade5e6e7c94752ebdb48
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-05 17:22:39 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,9 +1,9 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name 
       t.string :location
-      t.integer :costume_inventory
+      t.string :theme
       t.integer :num_of_employees
       t.boolean  :still_in_business
       t.datetime :opening_time
